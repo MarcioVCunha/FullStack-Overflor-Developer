fc40b3d28f08ea36c2c352df78797dbab5a67abf
Marcio <marcioabcd11@gmail.com>
2021-12-13 12:58:14 -0300

    readme created

--- database.sql
+++ database.sql
@@ -1,48 +1,283 @@
-CREATE TABLE "questions" (
-	"id" serial NOT NULL,
-	"student" integer,
-	"question" TEXT NOT NULL,
-	"class" integer NOT NULL,
-	"tags" TEXT NOT NULL,
-	"answered" bool NOT NULL DEFAULT 'false',
-	"submitAt" TEXT NOT NULL,
-	"answeredAt" TEXT,
-	"answeredBy" integer,
-	"answer" TEXT,
-	CONSTRAINT "questions_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+-- Dumped by pg_dump version 12.9 (Ubuntu 12.9-0ubuntu0.20.04.1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', 'public', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: classes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE classes (
+    id integer NOT NULL,
+    "className" text NOT NULL
 );
 
 
+ALTER TABLE classes OWNER TO postgres;
 
-CREATE TABLE "students" (
-	"id" serial NOT NULL UNIQUE,
-	"token" uuid NOT NULL UNIQUE,
-	"name" TEXT NOT NULL UNIQUE,
-	CONSTRAINT "students_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+--
+-- Name: classes_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE classes_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE classes_id_seq OWNER TO postgres;
+
+--
+-- Name: classes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE classes_id_seq OWNED BY classes.id;
+
+
+--
+-- Name: questions; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE questions (
+    id integer NOT NULL,
+    student integer NOT NULL,
+    question text NOT NULL,
+    class integer NOT NULL,
+    tags text NOT NULL,
+    answered boolean DEFAULT false NOT NULL,
+    "submitAt" text NOT NULL,
+    "answeredAt" text,
+    "answeredBy" integer,
+    answer text
 );
 
 
+ALTER TABLE questions OWNER TO postgres;
 
-CREATE TABLE "classes" (
-	"id" serial NOT NULL,
-	"className" TEXT NOT NULL UNIQUE,
-	CONSTRAINT "classes_pk" PRIMARY KEY ("id")
-) WITH (
-  OIDS=FALSE
+--
+-- Name: questions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE questions_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE questions_id_seq OWNER TO postgres;
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE questions_id_seq OWNED BY questions.id;
+
+
+--
+-- Name: students; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE students (
+    id integer NOT NULL,
+    token uuid NOT NULL,
+    name text NOT NULL,
+    "classId" integer NOT NULL
 );
 
 
+ALTER TABLE students OWNER TO postgres;
 
-ALTER TABLE "questions" ADD CONSTRAINT "questions_fk0" FOREIGN KEY ("student") REFERENCES "students"("id");
-ALTER TABLE "questions" ADD CONSTRAINT "questions_fk1" FOREIGN KEY ("class") REFERENCES "classes"("id");
-ALTER TABLE "questions" ADD CONSTRAINT "questions_fk2" FOREIGN KEY ("answeredBy") REFERENCES "students"("id");
+--
+-- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE students_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
 
+ALTER TABLE students_id_seq OWNER TO postgres;
+
+--
+-- Name: students_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE students_id_seq OWNED BY students.id;
 
 
+--
+-- Name: classes id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY classes ALTER COLUMN id SET DEFAULT nextval('classes_id_seq'::regclass);
 
 
+--
+-- Name: questions id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY questions ALTER COLUMN id SET DEFAULT nextval('questions_id_seq'::regclass);
+
+
+--
+-- Name: students id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY students ALTER COLUMN id SET DEFAULT nextval('students_id_seq'::regclass);
+
+
+--
+-- Data for Name: classes; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY classes (id, "className") FROM stdin;
+\.
+
+
+--
+-- Data for Name: questions; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY questions (id, student, question, class, tags, answered, "submitAt", "answeredAt", "answeredBy", answer) FROM stdin;
+\.
+
+
+--
+-- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY students (id, token, name, "classId") FROM stdin;
+\.
+
+
+--
+-- Name: classes_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('classes_id_seq', 18, true);
+
+
+--
+-- Name: questions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('questions_id_seq', 7, true);
+
+
+--
+-- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('students_id_seq', 7, true);
+
+
+--
+-- Name: classes classes_className_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY classes
+    ADD CONSTRAINT "classes_className_key" UNIQUE ("className");
+
+
+--
+-- Name: classes classes_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY classes
+    ADD CONSTRAINT classes_pk PRIMARY KEY (id);
+
+
+--
+-- Name: questions questions_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY questions
+    ADD CONSTRAINT questions_pk PRIMARY KEY (id);
+
+
+--
+-- Name: students students_name_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_name_key UNIQUE (name);
+
+
+--
+-- Name: students students_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_pk PRIMARY KEY (id);
+
+
+--
+-- Name: students students_token_key; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_token_key UNIQUE (token);
+
+
+--
+-- Name: questions questions_fk0; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY questions
+    ADD CONSTRAINT questions_fk0 FOREIGN KEY (student) REFERENCES students(id);
+
+
+--
+-- Name: questions questions_fk1; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY questions
+    ADD CONSTRAINT questions_fk1 FOREIGN KEY (class) REFERENCES classes(id);
+
+
+--
+-- Name: questions questions_fk2; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY questions
+    ADD CONSTRAINT questions_fk2 FOREIGN KEY ("answeredBy") REFERENCES students(id);
+
+
+--
+-- Name: students students_fk0; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY students
+    ADD CONSTRAINT students_fk0 FOREIGN KEY ("classId") REFERENCES classes(id);
+
+
+--
+-- PostgreSQL database dump complete
+--
